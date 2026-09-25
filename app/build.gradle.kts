2ee117abcf01db6fba795258cfe984f7cc5e906f
Nestor-futbolia <nestorzuniga1986@gmail.com>
2026-09-25 01:20:44 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-android { namespace = "com.futbolia.app"; compileSdk = 35
-    defaultConfig { applicationId = "com.futbolia.app"; minSdk = 24; targetSdk = 35; versionCode = 20; versionName = "2.0.0" }
-}
+
+android {
+    namespace = "com.futbolia.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.futbolia.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+} 

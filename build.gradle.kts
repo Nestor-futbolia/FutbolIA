6e78ea1ad2e9dc8527d2e25e91461f4ec42cba78
Nestor-futbolia <nestorzuniga1986@gmail.com>
2026-09-25 00:31:40 -0500

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
-android { namespace = "com.futbolia.app"; compileSdk = 35
-    defaultConfig { applicationId = "com.futbolia.app"; minSdk = 24; targetSdk = 35; versionCode = 20; versionName = "2.0.0" }
-}
